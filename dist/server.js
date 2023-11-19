879e9ba871aa9ab9ade6035fab74e9ee543d35df
Allan Soares Amaral <allan-cebola@hotmail.com>
2023-11-19 18:47:25 -0300

    adiciona cors e pasta com comandos

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,3 @@
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; } const _app = require('./app');
-
-const _app2 = _interopRequireDefault(_app);
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
 _app2.default.listen(process.env.APP_PORT);
--- src/app.js
+++ src/app.js
@@ -6,11 +6,28 @@ dotenv.config();
 import './database';
 import express from 'express';
 
+import cors from 'cors';
+import helmet from 'helmet';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import photoRoutes from './routes/photoRoutes';
+
+const whiteList = [
+  'https://api.queirubim.online',
+  'http://localhost:3001',
+];
+
+const corsOptions = {
+  origin(origin, cb) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      cb(null, true);
+    } else {
+      cb(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -20,6 +37,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
