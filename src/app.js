670314ec5918ceb52fefd922addfeb4a646fe7d6
Allan Soares Amaral <allan-cebola@hotmail.com>
2023-11-25 02:59:57 -0300

    remove helmet

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ import './database';
 import express from 'express';
 
 import cors from 'cors';
-import helmet from 'helmet';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
@@ -16,7 +15,7 @@ import photoRoutes from './routes/photoRoutes';
 
 const whiteList = [
   'https://api.queirubim.online',
-  'http://localhost:3001',
+  'http://localhost:3000',
 ];
 
 const corsOptions = {
@@ -38,7 +37,6 @@ class App {
 
   middlewares() {
     this.app.use(cors(corsOptions));
-    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
